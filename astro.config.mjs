2bfb07363f5a085fa38ac1c75183d7d38688b3f4
Max Blancarte <blancarte@gmail.com>
2022-08-09 18:23:47 -0700

    feat: partytown test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,32 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        resolveUrl: function (url, location, type) {
+          if (url.hostname === "connect.facebook.net") {
+            const proxyUrl = new URL(
+              "https://cdn.builder.codes/api/v1/js-proxy"
+            );
+            proxyUrl.searchParams.append("url", url.href);
+            proxyUrl.searchParams.append(
+              "apiKey",
+              abfebca9fdcf4536ac17a116edb9e66b
+            );
+            console.log(proxyUrl.href);
+            return proxyUrl;
+          }
+
+          return url;
+        },
+        forward: ["fbq"],
+      },
+    }),
+  ],
 });
